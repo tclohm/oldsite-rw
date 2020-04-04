1ce553c430d0f9252ac5c4f14dd18b53f46e2669
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-04-03 20:43:45 -0700

    added css to bloglayout and added bloglayout to homepage, projectpage, and writingpage

--- web/src/pages/ProjectsPage/ProjectsPage.js
+++ web/src/pages/ProjectsPage/ProjectsPage.js
@@ -1,14 +1,19 @@
 import { Link, routes } from "@redwoodjs/router"
+
 import { Breadcrumb, BreadcrumbItem } from "reactstrap"
+
+import BlogLayout from "src/layouts/BlogLayout"
 
 const ProjectPage = () => {
   return (
-  	<div>
-    	<Breadcrumb>
-  			<BreadcrumbItem><Link to={routes.home()}>Home</Link></BreadcrumbItem>
-  			<BreadcrumbItem active>Projects</BreadcrumbItem>
-  		</Breadcrumb>
-    </div>
+  	<BlogLayout>
+	  	<div>
+	    	<Breadcrumb>
+	  			<BreadcrumbItem><Link to={routes.home()}>Home</Link></BreadcrumbItem>
+	  			<BreadcrumbItem active>Projects</BreadcrumbItem>
+	  		</Breadcrumb>
+	    </div>
+    </BlogLayout>
   )
 }
 
--- web/src/pages/WritingsPage/WritingsPage.js
+++ web/src/pages/WritingsPage/WritingsPage.js
@@ -1,9 +1,19 @@
+import { Link, routes } from "@redwoodjs/router"
+
+import { Breadcrumb, BreadcrumbItem } from "reactstrap"
+
+import BlogLayout from "src/layouts/BlogLayout"
+
 const WritingPage = () => {
   return (
-    <div>
-      <h1>WritingPage</h1>
-      <p>Find me in ./web/src/pages/WritingsPage/WritingsPage.js</p>
-    </div>
+	<BlogLayout>
+	  	<div>
+	    	<Breadcrumb>
+	  			<BreadcrumbItem><Link to={routes.home()}>Home</Link></BreadcrumbItem>
+	  			<BreadcrumbItem active>Writing</BreadcrumbItem>
+	  		</Breadcrumb>
+	    </div>
+    </BlogLayout>
   )
 }
 
