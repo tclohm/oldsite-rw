1e3e627f6a84a7b3285bf866237dc5d079c3fb1c
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-06-29 15:22:43 -0700

    added bootstrap-icons and adjustments to contact page and layout

--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -6,32 +6,37 @@ import { useSessionStorage } from "../../hooks/useSessionStorage"
 const BlogLayout = ({ children }) => {
 
   const [isOpen, setIsOpen] = useSessionStorage("linksOpen", false);
-  const [isActive, setIsActive] = useSessionStorage("active", false)
-  const [onProject, setOnProject] = useSessionStorage("onProject", false)
-  const [onWriting, setOnWriting] = useSessionStorage("onWriting", false)
+  const [onContact, setOnContact] = useSessionStorage("onContact", false);
+  const [onProject, setOnProject] = useSessionStorage("onProject", false);
+  const [onWriting, setOnWriting] = useSessionStorage("onWriting", false);
+  const [onHome, setOnHome] = useSessionStorage("onHome", true);
 
   function toggle(event) {
     console.log(event.currentTarget.name)
     switch (event.currentTarget.name) {
       case "links":
         const contactBtn = document.querySelector('.contact-links')
-        setIsActive(!isActive);
         setIsOpen(!isOpen)
-        isOpen ? contactBtn.className = 'mr-1 contact-links btn btn-primary' : contactBtn.className = 'mr-1 contact-links btn btn-primary active'
         break
       case "projects":
-        const projectBtn = document.querySelector('.projects')
         if (onWriting) { setOnWriting(!onWriting) }
+        if (onContact) { setOnContact(!onContact) }
         if (!onProject) { setOnProject(!onProject) }
-        onProject ? projectBtn.className = 'inline projects btn btn-primary' : projectBtn.className = 'inline projects btn btn-primary outline'
         break
       case "writings":
-        const writingBtn = document.querySelector('.writings')
         if (onProject) { setOnProject(!onProject) }
+        if (onContact) { setOnContact(!onContact) }
         if (!onWriting) { setOnWriting(!onWriting) }
-        onWriting ? writingBtn.className = 'inline ml-3 writings btn btn-primary outline' : writingBtn.className = 'inline ml-3 writings btn btn-primary'
+        break
+      case "email":
+        if (onWriting) { setOnWriting(!onWriting) } 
+        if (onProject) { setOnProject(!onProject) }
+        if (!onContact) { setOnContact(!onContact) }
         break
       default:
+        if (onWriting) { setOnWriting(!onWriting) } 
+        if (onProject) { setOnProject(!onProject) }
+        if (onContact) { setOnContact(!onContact) }
         break
     }
   }
@@ -42,23 +47,48 @@ const BlogLayout = ({ children }) => {
   <Container>
         <Row className="mt-5">
           <Col>
-            <h1><Link to="/" className="hidden-link"><i className="far fa-hand-paper swing"></i></Link></h1>
+            <h1><Link to="/" className="hidden-link"><i className="far fa-hand-paper swing home" onClick={toggle}></i></Link></h1>
             <h1>Hey there, my name is <span>Taylor</span></h1>
             <p>Web and iOS Developer</p>
           </Col>
           <Col xs={{ offset: 1 }}>
             <Link to={routes.contact()}>
-              <Button outline color="primary" className="inline mr-3">
-                <i className="fas fa-envelope"></i> Me
-              </Button>
+              {onContact ?
+                <Button 
+                  color="primary" 
+                  className="inline mr-3 email"
+                  onClick={toggle}
+                  name="email">
+                  <i className="fas fa-envelope"></i> Me
+                </Button>
+                :
+                <Button 
+                  outline color="primary" 
+                  className="inline mr-3 email"
+                  onClick={toggle}
+                  name="email">
+                  <i className="fas fa-envelope"></i> Me
+                </Button>
+              }
             </Link>
-            <Button 
-              color="primary" 
+            {isOpen ?
+              <Button 
+              color="info"
+              active 
+              onClick={toggle} 
+              name="links" 
+              className="mr-1 contact-links">
+              <i className="fas fa-link"></i>
+              </Button>
+              :
+              <Button 
+              color="info" 
               onClick={toggle} 
               name="links" 
               className="mr-1 contact-links">
               <i className="fas fa-link"></i>
             </Button>
+            }
             <Col xs={{ offset: 7 }}>
             <Collapse isOpen={isOpen}>
               <Row>
--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -9,6 +9,8 @@ import {
 	FormError
 } from "@redwoodjs/web"
 
+import { Button, Row, Col, FormFeedback } from "reactstrap"
+
 import { useForm } from "react-hook-form"
 
 import BlogLayout from "src/layouts/BlogLayout"
@@ -38,49 +40,82 @@ const ContactPage = (props) => {
 
 	return ( 
 		<BlogLayout>
-			<Form 
-				onSubmit={onSubmit} 
-				validation={{ mode: "onBlur" }} 
-				error={error}
-				formMethods={formMethods}>
-				<FormError
+				<Form 
+					onSubmit={onSubmit} 
+					validation={{ mode: "onBlur" }} 
 					error={error}
-					wrapperStyle={{ color: "red", backgroundColor: "lavenderblush" }} 
-				/>
-				<Label name="name" errorClassName="error">
-					Name
-				</Label>
-				<TextField 
-					name="name"
-					validation={{ required: true }} 
-					errorClassName="error"
-				/>
-				<FieldError name="name" className="error" />
+					formMethods={formMethods}>
+					<Row>
+						<Col xs={{ size: 10 }}>
+							<FormError
+									error={error}
+									wrapperStyle={{ color: "red", backgroundColor: "lavenderblush" }} 
+								/>
+						</Col>
+					</Row>
+					<Row className="mb-3">
+						<Col xs={{ offset: 4 }}>
+							<Label 
+								name="name" 
+								errorClassName="error"
+								style={{ display: 'block' }}>
+								Name
+							</Label>
+							<TextField 
+								name="name"
+								validation={{ required: "Please enter your name" }} 
+								errorClassName="error"
+							/>
+							<FieldError name="name" className="error"/>
+						</Col>
+					</Row>
+					<Row className="mb-3">
+						<Col xs={{ offset: 4 }}>
+						<Label 
+							name="email" 
+							errorClassName="error" 
+							style={{ display: 'block' }}>
+							Email
+						</Label>
+						<TextField 
+							name="email" 
+							validation={{ 
+								required: "Please enter your email",
+							}} 
+							errorClassName="error"
+						/>
+						<FieldError name="message"/>
+						</Col>
+					</Row>
+					<Row>
+						<Col xs={{ offset: 4 }}>
+						<Label 
+							name="message" 
+							errorClassName="error" 
+							style={{ display: 'block' }}>
+							Message
+						</Label>
+						<TextAreaField 
+							name="message" 
+							validation={{ required: 'Please enter a message' }}
+							errorClassName="error"
+						/>
+						<FieldError name="message" />
+						</Col>
+					</Row>
+					<Row>
+						<Col xs={{ offset: 4 }}>
+						<Button color="success">
+							<Submit
 
-				<Label name="email" errorClassName="error">
-					Email
-				</Label>
-				<TextField 
-					name="email" 
-					validation={{ 
-						required: true,
-					}} 
-					errorClassName="error"
-				/>
-				<FieldError name="email" className="error" />
-
-				<Label name="message" errorClassName="error">
-					Message
-				</Label>
-				<TextAreaField 
-					name="message" 
-					validation={{ required: true }}
-					errorClassName="error"
-				/>
-				<FieldError name="message" style={{ color: "red" }} />
-
-				<Submit>Save</Submit>
-			</Form>
+								style={{ backgroundColor: 'transparent', border: '0px' }}>
+									<i className="far fa-paper-plane"style={{ color: 'white' }}>
+									</i>
+							</Submit>
+						</Button>
+						</Col>
+					</Row>
+				</Form>
 		</BlogLayout>
 	)
 }
