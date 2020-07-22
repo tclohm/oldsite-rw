cc724e9c6cd433a4cabb27d18a067ef5be4f031d
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-07-21 22:10:25 -0700

    added css to homepage

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -5,6 +5,30 @@ const HomePage = () => {
 	return (
 		<>
 		<BlogLayout></BlogLayout>
+		<div className="illustration">
+			<div className="center">
+				<div className="circle index-1"></div>
+				<div className="circle index-2"></div>
+				<div className="circle index-3"></div>
+				<div className="circle index-4"></div>
+				<div className="circle index-5"></div>
+				<div className="disc index-1">
+					<div className="inner"></div>
+				</div>
+				<div className="disc index-2">
+					<div className="inner"></div>
+				</div>
+				<div className="disc index-3">
+					<div className="inner"></div>
+				</div>
+				<div className="disc index-4">
+					<div className="inner"></div>
+				</div>
+				<div className="disc index-5">
+					<div className="inner"></div>
+				</div>
+			</div>
+		</div>
 		</>
   	)
 }
